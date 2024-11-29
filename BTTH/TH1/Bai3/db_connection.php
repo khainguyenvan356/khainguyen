0bbe6dbf9cd588e831624cd1bce67eb569fdc9d3
khainguyenvan356 <144759920+khainguyenvan356@users.noreply.github.com>
2024-11-30 02:29:59 +0700

    Add files via upload

---
+++ BTTH/TH1/Bai3/db_connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "student_db"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+?>
